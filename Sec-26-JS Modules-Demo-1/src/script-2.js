6321c65601afcc0bf814c03e51e5646374fd4473
Shivam Baghel SBC1 <shivam3baghel@gmail.com>
2025-12-24 14:09:41 +0530

    Commit on 24 12 2025

--- Sec-26-JS Modules-Demo-1/src/script-2.js
+++ Sec-26-JS Modules-Demo-1/src/script-2.js
@@ -1,4 +1,4 @@
 import { add } from './script-1.js';
 let result=add(1,2);
-window.document.querySelector("#p1").textContent=result;
+document.querySelector("#p1").textContent=result;
 console.log(result);
---
+++ Sec-26-JS Modules/Demo/person.js
@@ -0,0 +1,2 @@
+export const name="Shivam Singh Baghel";
+export const age=28;
