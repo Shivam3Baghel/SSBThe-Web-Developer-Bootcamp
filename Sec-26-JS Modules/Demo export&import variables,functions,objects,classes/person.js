041dc229fdf65b55d169d104776f59f8080d3b60
Shivam Baghel SBC1 <shivam3baghel@gmail.com>
2026-01-03 20:31:57 +0530

    commit on 3 1 2026

---
+++ Sec-26-JS Modules/Demo export&import variables,functions,objects,classes/person.js
@@ -0,0 +1,7 @@
+export const name="Shivam Singh Baghel";
+export const age=28;
+export const say=()=>{console.log(0); return 1;};
+export const person1={
+    name:"Shivam Singh Baghel",
+    speak:()=>{console.log(2); return 3;}
+}
--- Sec-26-JS Modules/Demo/person.js
+++
@@ -1,3 +0,0 @@
-export const name="Shivam Singh Baghel";
-export const age=28;
-export const say=()=>{console.log(0); return 1;};
