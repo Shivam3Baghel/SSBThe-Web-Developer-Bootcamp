91e5fd7c4610e00870355bde5ca9d76c80142d21
Shivam Baghel SBC1 <shivam3baghel@gmail.com>
2026-03-28 21:10:49 +0530

    commit 28 3 2026

---
+++ Sec-14-JS Basics/script.js
@@ -0,0 +1,44 @@
+const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer',"gunners"];
+for (let i = 0 ; i <=subreddits.length-1; i++) {
+console.log(`Visit reddit.com/r/${subreddits[i]}`);
+}
+for (let sub of subreddits) {
+console.log(sub);
+}
+for(let char of "Shivam Singh Baghel"){
+    console.log(char);
+}
+// Output-
+// Visit reddit.com/r/cringe
+// Visit reddit.com/r/books
+// Visit reddit.com/r/chickens
+// Visit reddit.com/r/funny
+// Visit reddit.com/r/pics
+// Visit reddit.com/r/soccer
+// Visit reddit.com/r/gunners
+// cringe
+// books
+// chickens
+// funny
+// pics
+// soccer
+// gunners
+// S
+// h
+// i
+// v
+// a
+// m
+ 
+// S
+// i
+// n
+// g
+// h
+ 
+// B
+// a
+// g
+// h
+// e
+// l
