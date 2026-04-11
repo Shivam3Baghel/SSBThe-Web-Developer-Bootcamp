2c6895b2c001a329ef4a3c94886970faf1e818d8
Shivam Baghel SBC1 <shivam3baghel@gmail.com>
2026-04-11 12:38:29 +0530

    11 4 2026 0

--- Sec-20-JS Arrays-/Demo4 every some reduce arrow functions and this/script.js
+++ Sec-20-JS Arrays-/Demo4 every some reduce arrow functions and this/script.js
@@ -10,6 +10,37 @@ console.log(y);
 const nums=[1,4,6,8];
 let z=nums.reduce((x,y)=>{return x+y},100);
 console.log(z);
+const personWhichHasNormalFunction={
+    firstName:"Shivam",
+    lastName:"Singh Baghel",
+    fullName:function(){
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+console.log(personWhichHasNormalFunction.fullName());
+
+
+const personWhichHasArrowFunctionWithCurlyBrackets={
+    firstName:"Shivam",
+    lastName:"Singh Baghel",
+    fullName:()=>{
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+console.log(personWhichHasArrowFunctionWithCurlyBrackets.fullName());
+
+
+const personWhichHasArrowFunctionwithoutCurlyBrackets={
+    firstName:"Shivam",
+    lastName:"Singh Baghel",
+    fullName:()=>
+        `${this.firstName} ${this.lastName}`
+}
+console.log(personWhichHasArrowFunctionwithoutCurlyBrackets.fullName());
+//O/P-
 // 35
 // 35
 // 119
+// Shivam Singh Baghel
+// undefined undefined
+// undefined undefined
